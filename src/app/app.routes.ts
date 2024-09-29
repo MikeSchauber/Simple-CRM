c2857ebda2abeeef9c834a5d1c0c59a8c8d71a6f
Mike Schauber <mike.schauber96@gmail.com>
2024-09-29 19:01:56 +0200

    create 3 more menu components and set routing to those

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
+import { OrganisationsComponent } from './components/organisations/organisations.component';
+import { TicketsComponent } from './components/tickets/tickets.component';
+import { FaqComponent } from './components/faq/faq.component';
 
 export const routes: Routes = [
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'contacts', component: ContactsComponent }
+  { path: '', component: DashboardComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'organisations', component: OrganisationsComponent },
+  { path: 'tickets', component: TicketsComponent },
+  { path: 'faq', component: FaqComponent },
 ];
---
+++ src/app/components/organisations/organisations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organisations',
+  standalone: true,
+  imports: [],
+  templateUrl: './organisations.component.html',
+  styleUrl: './organisations.component.scss'
+})
+export class OrganisationsComponent {
+
+}
