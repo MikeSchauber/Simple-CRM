8a89df49b0fefe49c574f4eac9d272d70335a0e8
Mike Schauber <mike.schauber96@gmail.com>
2024-09-30 17:19:52 +0200

    visible inputs

--- src/app/components/contacts/table/table.component.ts
+++ src/app/components/contacts/table/table.component.ts
@@ -28,59 +28,17 @@ export class TableComponent {
       role: 'Dj',
       tel: '0151319023',
       email: 'mike.schauber@gmx.de',
-    },
-    {
-      checked: false,
-      name: 'Hydrogen',
-      role: 'Dj',
-      tel: '0151319023',
-      email: 'mike.schauber@gmx.de',
-    },
-    {
-      checked: false,
-      name: 'Hydrogen',
-      role: 'Dj',
-      tel: '0151319023',
-      email: 'mike.schauber@gmx.de',
-    },
-    {
-      checked: false,
-      name: 'Hydrogen',
-      role: 'Dj',
-      tel: '0151319023',
-      email: 'mike.schauber@gmx.de',
-    },
-    {
-      checked: false,
-      name: 'Hydrogen',
-      role: 'Dj',
-      tel: '0151319023',
-      email: 'mike.schauber@gmx.de',
-    },
-    {
-      checked: false,
-      name: 'Hydrogen',
-      role: 'Dj',
-      tel: '0151319023',
-      email: 'mike.schauber@gmx.de',
-    },
-    {
-      checked: false,
-      name: 'Hydrogen',
-      role: 'Dj',
-      tel: '0151319023',
-      email: 'mike.schauber@gmx.de',
-    },
-    {
-      checked: false,
-      name: 'Hydrogen',
-      role: 'Dj',
-      tel: '0151319023',
-      email: 'mike.schauber@gmx.de',
-    },
+    }
   ];
   displayedColumns: string[] = ['checked', 'name', 'role', 'telefon'];
   allChecked: boolean = false;
+  newContact: ContactsTable = {
+    checked: false,
+    name: '',
+    role: '',
+    tel: '',
+    email: '',
+  }
 
   constructor() {}
 
@@ -96,5 +54,21 @@ export class TableComponent {
         e.checked = false;
       });
     }
+  }
+
+  addContact($event: KeyboardEvent) {
+    console.log($event.keyCode);
+    if ($event.keyCode === 13) {
+      this.contactsData.push(
+        {
+          checked: false,
+          name: this.newContact.name,
+          role: this.newContact.role,
+          tel: this.newContact.tel,
+          email: this.newContact.email,
+        }
+      );	
+    }
+    
   }
 }
