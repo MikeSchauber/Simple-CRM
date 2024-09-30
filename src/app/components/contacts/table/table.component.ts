087a2826065eac0829436a6f8f01e845b204d1d4
Mike Schauber <mike.schauber96@gmail.com>
2024-09-30 16:59:02 +0200

    got a good table with inputs in there

--- src/app/components/contacts/table/table.component.ts
+++ src/app/components/contacts/table/table.component.ts
@@ -1,37 +1,88 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { ContactsTable } from '../../../interfaces/contactsTable';
-
 import { FormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [MatButtonModule, MatCheckboxModule, FormsModule, MatIconModule, CommonModule],
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatIconModule,
+    CommonModule,
+  ],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
 })
 export class TableComponent {
   contactsData: ContactsTable[] = [
-    {checked: false, name: 'Hydrogen', role: 'Dj', tel: '0151319023', email: "mike.schauber@gmx.de"},
-    {checked: false, name: 'Hydrogen', role: 'Dj', tel: '0151319023', email: "mike.schauber@gmx.de"},
-    {checked: false, name: 'Hydrogen', role: 'Dj', tel: '0151319023', email: "mike.schauber@gmx.de"},
-    {checked: false, name: 'Hydrogen', role: 'Dj', tel: '0151319023', email: "mike.schauber@gmx.de"},
-    {checked: false, name: 'Hydrogen', role: 'Dj', tel: '0151319023', email: "mike.schauber@gmx.de"},
-    {checked: false, name: 'Hydrogen', role: 'Dj', tel: '0151319023', email: "mike.schauber@gmx.de"},
-    {checked: false, name: 'Hydrogen', role: 'Dj', tel: '0151319023', email: "mike.schauber@gmx.de"},
-    {checked: false, name: 'Hydrogen', role: 'Dj', tel: '0151319023', email: "mike.schauber@gmx.de"},
-    
+    {
+      checked: false,
+      name: 'Hydrogen',
+      role: 'Dj',
+      tel: '0151319023',
+      email: 'mike.schauber@gmx.de',
+    },
+    {
+      checked: false,
+      name: 'Hydrogen',
+      role: 'Dj',
+      tel: '0151319023',
+      email: 'mike.schauber@gmx.de',
+    },
+    {
+      checked: false,
+      name: 'Hydrogen',
+      role: 'Dj',
+      tel: '0151319023',
+      email: 'mike.schauber@gmx.de',
+    },
+    {
+      checked: false,
+      name: 'Hydrogen',
+      role: 'Dj',
+      tel: '0151319023',
+      email: 'mike.schauber@gmx.de',
+    },
+    {
+      checked: false,
+      name: 'Hydrogen',
+      role: 'Dj',
+      tel: '0151319023',
+      email: 'mike.schauber@gmx.de',
+    },
+    {
+      checked: false,
+      name: 'Hydrogen',
+      role: 'Dj',
+      tel: '0151319023',
+      email: 'mike.schauber@gmx.de',
+    },
+    {
+      checked: false,
+      name: 'Hydrogen',
+      role: 'Dj',
+      tel: '0151319023',
+      email: 'mike.schauber@gmx.de',
+    },
+    {
+      checked: false,
+      name: 'Hydrogen',
+      role: 'Dj',
+      tel: '0151319023',
+      email: 'mike.schauber@gmx.de',
+    },
   ];
   displayedColumns: string[] = ['checked', 'name', 'role', 'telefon'];
   allChecked: boolean = false;
 
-  constructor() {
-
-  }
+  constructor() {}
 
   checkAllContacts() {
     if (!this.allChecked) {
